62f86a9e98e6ab995848964c574f0b071d374615
Niji <nijistore0109@gmail.com>
2026-04-28 15:54:26 -0600

    fix pool import

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
+const pool = require('./db');
 
 // ── APP ──
 const app  = express();
